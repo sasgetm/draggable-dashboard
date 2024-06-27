a1066f1a8df9ea0ed44d18a90af6946bff99c5cb
Alexander Getmanets <sasgetm@yandex.ru>
2024-06-27 18:03:12 +0700

    gh-pages-try-5

--- src/router/router.js
+++ src/router/router.js
@@ -23,7 +23,7 @@ const routes = [
 
 const router = createRouter({
   routes,
-  history: createWebHashHistory(),
+  history: createWebHistory(),
 });
 
 export default router;
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: '/draggable-dashboard/'
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   publicPath: '/draggable-dashboard/'
+// })
+
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/draggable-dashboard/'
+    : '/'
+}
